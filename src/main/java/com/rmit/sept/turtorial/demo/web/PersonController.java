237bbeccbadf961e7d8a6c9dcb04ddb85ecffd4a
Ash <Cassy2161>
2020-08-21 08:43:40 +1000

    Personservice commit

--- src/main/java/com/rmit/sept/turtorial/demo/web/PersonController.java
+++ src/main/java/com/rmit/sept/turtorial/demo/web/PersonController.java
@@ -6,12 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/person")
@@ -23,15 +27,32 @@ public class PersonController {
     @PostMapping("")
     public ResponseEntity<?> createNewPerson(@Valid @RequestBody Person person, BindingResult result) {
         if (result.hasErrors()){
-            return new ResponseEntity<String>("Invalid Person Object", HttpStatus.BAD_REQUEST);
+            Map<String, String> errorMap = new HashMap<>();
+            for (FieldError error: result.getFieldErrors()){
+                errorMap.put(error.getField(),error.getDefaultMessage());
+            }
+            return new ResponseEntity<List<FieldError>>(result.getFieldErrors(), HttpStatus.BAD_REQUEST);
         }
         Person person1 = personService.saveOrUpdatePerson(person);
         return new ResponseEntity<Person>(person, HttpStatus.CREATED);
     }
 }
 /*
+1
 public ResponseEntity<Person> createNewPerson(@RequestBody Person person) {
 
         Person person1 = personService.saveOrUpdatePerson(person);
-        return new ResponseEntity<Person>(person, HttpStatus.CREA
+        return new ResponseEntity<Person>(person, HttpStatus.CREATED);
+ */
+
+
+/*
+2
+public ResponseEntity<?> createNewPerson(@Valid @RequestBody Person person, BindingResult result) {
+        if (result.hasErrors()){
+            return new ResponseEntity<String>("Invalid Person Object", HttpStatus.BAD_REQUEST);
+        }
+        Person person1 = personService.saveOrUpdatePerson(person);
+        return new ResponseEntity<Person>(person, HttpStatus.CREATED);
+    }
  */
